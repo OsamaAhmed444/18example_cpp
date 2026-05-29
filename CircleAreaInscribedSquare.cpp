cddc30d01dd6c409d27c799b59288c7701be7aa5
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:22:18 +0300

    Update CircleAreaInscribedSquare.cpp

--- CircleAreaInscribedSquare.cpp
+++ CircleAreaInscribedSquare.cpp
@@ -1,12 +1,12 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	const float pi = 3.14;
-//	float Area;
-//
-//	cout << "Please Enter The Area of the Circle: ";
-//	cin >> Area;
-//	cout << "\n The Area of the Inscribed Square is: " << (pi * (Area * Area))/4 << "\n";
-//
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	const float pi = 3.14;
+	float Area;
+
+	cout << "Please Enter The Area of the Circle: ";
+	cin >> Area;
+	cout << "\n The Area of the Inscribed Square is: " << (pi * (Area * Area))/4 << "\n";
+
+}
