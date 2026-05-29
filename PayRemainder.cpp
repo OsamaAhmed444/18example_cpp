e800df19d71c4122f881d4197e6a61e7582da0b7
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:23:04 +0300

    Update PayRemainder.cpp

--- PayRemainder.cpp
+++ PayRemainder.cpp
@@ -1,16 +1,16 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	int TotalBill, PaidAmount;
-//
-//	cout << "Enter Total Bill: ";
-//	cin >> TotalBill;
-//	cout << "Enter Paid Amount: ";
-//	cin >> PaidAmount;
-//
-//	int Remainder = PaidAmount - TotalBill;
-//	cout << "Remainder Amount: " << Remainder << endl;
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	int TotalBill, PaidAmount;
+
+	cout << "Enter Total Bill: ";
+	cin >> TotalBill;
+	cout << "Enter Paid Amount: ";
+	cin >> PaidAmount;
+
+	int Remainder = PaidAmount - TotalBill;
+	cout << "Remainder Amount: " << Remainder << endl;
+
+	return 0;
+}
