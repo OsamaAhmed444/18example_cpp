3fc1a4a50adcabc559799e8e80e433d86ce96812
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:22:28 +0300

    Update CircleAreaThroughDiameter.cpp

--- CircleAreaThroughDiameter.cpp
+++ CircleAreaThroughDiameter.cpp
@@ -1,11 +1,11 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	const float pi = 3.14;
-//	float diameter;
-//
-//	cout << "Please Enter The Diameter of the Circle: ";
-//	cin >> diameter;
-//	cout << "\n The Area of the Circle is: " << pi * (diameter / 2.0) * (diameter / 2.0) << "\n";
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	const float pi = 3.14;
+	float diameter;
+
+	cout << "Please Enter The Diameter of the Circle: ";
+	cin >> diameter;
+	cout << "\n The Area of the Circle is: " << pi * (diameter / 2.0) * (diameter / 2.0) << "\n";
+}
