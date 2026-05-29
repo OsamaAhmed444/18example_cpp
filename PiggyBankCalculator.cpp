1cd872720ca11121919cc40734c2e4ebb1033b35
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:23:24 +0300

    Update PiggyBankCalculator.cpp

--- PiggyBankCalculator.cpp
+++ PiggyBankCalculator.cpp
@@ -1,23 +1,23 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	float penny, nickle, dime, quarter, dollar;
-//
-//	cout << "penny = 1 \nNickel = 5 \nDime = 10 \nQuarter = 25 \nDollar = 100 \n";
-//	cout << "Enter the number of pennies: ";
-//	cin >> penny;
-//	cout << "Enter the number of nickels: ";
-//	cin >> nickle;
-//	cout << "Enter the number of dimes: ";
-//	cin >> dime;
-//	cout << "Enter the number of quarters: ";
-//	cin >> quarter;
-//	cout << "Enter the number of dollars: ";
-//	cin >> dollar;
-//
-//	float total = penny * 1 + nickle * 5 + dime * 10 + quarter * 25 + dollar * 100;
-//	float dollars = total / 100;
-//	cout << "The total amount of money in the piggy bank is: " << dollars << " dollars and " << total << " cents." << endl;
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	float penny, nickle, dime, quarter, dollar;
+
+	cout << "penny = 1 \nNickel = 5 \nDime = 10 \nQuarter = 25 \nDollar = 100 \n";
+	cout << "Enter the number of pennies: ";
+	cin >> penny;
+	cout << "Enter the number of nickels: ";
+	cin >> nickle;
+	cout << "Enter the number of dimes: ";
+	cin >> dime;
+	cout << "Enter the number of quarters: ";
+	cin >> quarter;
+	cout << "Enter the number of dollars: ";
+	cin >> dollar;
+
+	float total = penny * 1 + nickle * 5 + dime * 10 + quarter * 25 + dollar * 100;
+	float dollars = total / 100;
+	cout << "The total amount of money in the piggy bank is: " << dollars << " dollars and " << total << " cents." << endl;
+	return 0;
+}
