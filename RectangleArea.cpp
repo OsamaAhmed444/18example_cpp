ea44399208c6bb071708d06f2c8863dfbe790a61
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:23:42 +0300

    Update RectangleArea.cpp

--- RectangleArea.cpp
+++ RectangleArea.cpp
@@ -1,11 +1,11 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	int hieght, width;
-//	cout << "Please Enter The Hieght of the Rectangle: ";
-//	cin >> hieght;
-//	cout << "Please Enter The Width of the Rectangle: ";
-//	cin >> width;
-//	cout << "\n The Area of the Rectangle is: " << hieght * width << "\n";
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	int hieght, width;
+	cout << "Please Enter The Hieght of the Rectangle: ";
+	cin >> hieght;
+	cout << "Please Enter The Width of the Rectangle: ";
+	cin >> width;
+	cout << "\n The Area of the Rectangle is: " << hieght * width << "\n";
+}
