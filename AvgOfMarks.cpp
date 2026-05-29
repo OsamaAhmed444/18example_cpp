7e78aaac1fada86a294e5bd279f904068f1cf788
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:21:44 +0300

    Restore AvgOfMarks.cpp with average calculation

--- AvgOfMarks.cpp
+++ AvgOfMarks.cpp
@@ -1,9 +1,9 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	int mark1, mark2, mark3;
-//	cout << "Please Enter Three Marks: ";
-//	cin >> mark1 >> mark2 >> mark3;
-//	cout << "\n The Average of " << mark1 << " + " << mark2 << " + " << mark3 << " is: " << (mark1 + mark2 + mark3) / 3.0 << "\n";
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	int mark1, mark2, mark3;
+	cout << "Please Enter Three Marks: ";
+	cin >> mark1 >> mark2 >> mark3;
+	cout << "\n The Average of " << mark1 << " + " << mark2 << " + " << mark3 << " is: " << (mark1 + mark2 + mark3) / 3.0 << "\n";
+}
