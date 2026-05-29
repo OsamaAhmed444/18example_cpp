74d00556a18e2825fc751a5106783b66058a1084
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:22:08 +0300

    Update CircleAreaInscribedIsoscelesTriangle.cpp

--- CircleAreaInscribedIsoscelesTriangle.cpp
+++ CircleAreaInscribedIsoscelesTriangle.cpp
@@ -1,12 +1,12 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	const double pi = 3.14;
-//	double a, b, area;
-//
-//	cout << "Enter the base and height of the triangle: ";
-//	cin >> a >> b;
-//	area = (pi * b * b / 4) * ((2 * a - b) / (2 * a + b));
-//	cout << "The area of the circle is: " << area << endl;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	const double pi = 3.14;
+	double a, b, area;
+
+	cout << "Enter the base and height of the triangle: ";
+	cin >> a >> b;
+	area = (pi * b * b / 4) * ((2 * a - b) / (2 * a + b));
+	cout << "The area of the circle is: " << area << endl;
+}
