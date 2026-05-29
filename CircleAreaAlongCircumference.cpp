71380f560c77d4999dd6c962f8ebf047a99bf6c1
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:21:57 +0300

    Restore CircleAreaAlongCircumference.cpp implementation

--- CircleAreaAlongCircumference.cpp
+++ CircleAreaAlongCircumference.cpp
@@ -1,12 +1,12 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	const float pi = 3.14;
-//	float l, area;
-//
-//	cout << "Enter the length of the circumference: ";
-//	cin >> l;
-//	area = (l * l) / (4 * pi);
-//	cout << "The area of the circle is: " << area << endl;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	const float pi = 3.14;
+	float l, area;
+
+	cout << "Enter the length of the circumference: ";
+	cin >> l;
+	area = (l * l) / (4 * pi);
+	cout << "The area of the circle is: " << area << endl;
+}
