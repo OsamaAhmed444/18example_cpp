14342f4b191989e975f6a900808567b9747b5bf2
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-05-29 15:22:56 +0300

    Update MonthlyLoanInstallment.cpp

--- MonthlyLoanInstallment.cpp
+++ MonthlyLoanInstallment.cpp
@@ -1,16 +1,16 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	int Months, LoanAmount;
-//
-//	cout << "Enter the Loan amount: ";
-//	cin >> LoanAmount;
-//	cout << "Enter the number of months: ";
-//	cin >> Months;
-//
-//	int MonthlyPayment = LoanAmount / Months;
-//	cout << "The monthly payment is: " << MonthlyPayment << endl;
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Months, LoanAmount;
+
+	cout << "Enter the Loan amount: ";
+	cin >> LoanAmount;
+	cout << "Enter the number of months: ";
+	cin >> Months;
+
+	int MonthlyPayment = LoanAmount / Months;
+	cout << "The monthly payment is: " << MonthlyPayment << endl;
+
+	return 0;
+}
